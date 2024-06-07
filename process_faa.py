3a8729a65b36d8dc3cd0941a021dccd7d1492cb9
sunningmbzuai <ning.sun@mbzuai.ac.ae>
2024-06-08 01:52:56 +0400

    add faa processing prints

--- process_faa.py
+++ process_faa.py
@@ -9,8 +9,13 @@ args = parser.parse_args()
 
 cols = ['proteinID', 'proteinSequence']
 rows = []
-for file in args.files:
-    proteins = Fasta(file, read_long_names=True)
+for i, file in enumerate(args.files):
+    print(len(args.files), i, end='\r')
+    try:
+        proteins = Fasta(file, read_long_names=True)
+    except:
+        continue
     for key, seq in proteins.items():
         rows.append([key, seq])
+print()
 pd.DataFrame(data=rows, columns=cols).to_csv(args.output, index=False, header=False, sep='\t')
